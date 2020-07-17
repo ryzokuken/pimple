69344b5e77a75bea167e38ae50309dce9f7e1740
Ujjwal Sharma <ryzokuken@disroot.org>
2020-07-17 10:17:56 +0530

    src: add weekday grid

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,18 @@ fn main() {
         window.set_title("Pimple");
         window.set_default_size(350, 70);
 
-        let button = Button::with_label("Click me!");
-        button.connect_clicked(|_| {
-            println!("Clicked!");
-        });
-        window.add(&button);
+        let b_main = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        let l_main = gtk::Label::new(Some("Pimple"));
+        let b_calendar = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+        for day in 0..7 {
+            let b_day = gtk::Box::new(gtk::Orientation::Vertical, 0);
+            let l_day = gtk::Label::new(Some(day.to_string().as_ref()));
+            b_day.add(&l_day);
+            b_calendar.pack_end(&b_day, true, false, 0);
+        }
+        b_main.pack_start(&l_main, false, false, 0);
+        b_main.pack_end(&b_calendar, true, false, 0);
+        window.add(&b_main);
 
         window.show_all();
     });
