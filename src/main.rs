c1c9d588c2e9bfcae5af2f193970b57ef43500d6
Ujjwal Sharma <ryzokuken@disroot.org>
2020-07-17 10:17:33 +0530

    src: clean up gtk code

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,15 @@ extern crate gio;
 use gtk::prelude::*;
 use gio::prelude::*;
 
-use gtk::{Application, ApplicationWindow, Button};
-
 fn main() {
-    let application = Application::new(
-        Some("com.github.gtk-rs.examples.basic"),
+    let application = gtk::Application::new(
+        Some("dev.ryzokuken.pimple"),
         Default::default(),
-    ).expect("failed to initialize GTK application");
+    ).expect("failed to initialize gtk application");
 
     application.connect_activate(|app| {
-        let window = ApplicationWindow::new(app);
-        window.set_title("First GTK+ Program");
+        let window = gtk::ApplicationWindow::new(app);
+        window.set_title("Pimple");
         window.set_default_size(350, 70);
 
         let button = Button::with_label("Click me!");
